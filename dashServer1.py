2853b70e0de943426231c8ef79e409473a518469
aspiringguru <bmatthewtaylor@gmail.com>
2019-11-18 11:09:42 +1000

    added styling

--- dashServer1.py
+++ dashServer1.py
@@ -15,12 +15,26 @@ external_stylesheets = ['dashServer1.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+
+app.layout = html.Div(children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+
+    html.Div(children='Dash: A web application framework for Python.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
 
     dcc.Graph(
         id='example-graph',
@@ -30,7 +44,11 @@ app.layout = html.Div(children=[
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
             }
         }
     )
